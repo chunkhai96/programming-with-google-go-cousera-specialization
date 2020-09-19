f1ce5204df7f5c795dd9446fae35258598c71a3e
Andy Pang <chunkhai96@gmail.com>
2020-09-19 13:14:04 +0800

    Add Course 3 Module 3

---
+++ 3-2-1-goroutine.go
@@ -0,0 +1,19 @@
+package main
+import (
+	"fmt"
+	"time"
+)
+
+func run(str string){
+	for i:=0; i<100; i++ {
+		fmt.Printf("%s: %d\n", str, i)
+	}
+}
+
+func main() {
+	// goroutine allows program to run concurrently
+	go run("goroutine 1")
+	go run("goroutine 2")
+
+	time.Sleep(time.Second)
+}
